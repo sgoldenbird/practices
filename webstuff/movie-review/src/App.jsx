cd9822e1d6849a8eefa25bbde9c1f7d937d01d03
Sienna <sgoldenbird@gmail.com>
2025-04-01 23:14:48 +0900

    feat: create review list

--- webstuff/movie-review/src/App.jsx
+++ webstuff/movie-review/src/App.jsx
@@ -1,10 +1,28 @@
+import { useState } from "react";
 import ReviewList from "./components/ReviewList.jsx";
-import items from "./data/items.json";
+import mockItems from "./data/items.json";
 
 function App() {
+  const [order, setOrder] = useState("createdAt");
+  const [items, setItems] = useState(mockItems);
+  const sortedItems = items.sort((a, b) => b[order] - a[order]);
+
+  const handleNewestClick = () => setOrder("createdAt");
+
+  const handleBestClick = () => setOrder("rating");
+
+  const handleDelete = (id) => {
+    const nextItems = items.filter((item) => item.id !== id);
+    setItems(nextItems);
+  };
+
   return (
     <div>
-      <ReviewList items={items} />
+      <div>
+        <button onClick={handleNewestClick}>최신순</button>
+        <button onClick={handleBestClick}>베스트순</button>
+      </div>
+      <ReviewList items={sortedItems} onDelete={handleDelete} />
     </div>
   );
 }
