1a2afbc1cb696786051b3a3b9d6698525327d4c0
Sienna <sgoldenbird@gmail.com>
2025-04-02 09:49:15 +0900

    feat: add items loading button

--- webstuff/foodit/src/App.jsx
+++ webstuff/foodit/src/App.jsx
@@ -1,10 +1,10 @@
 import { useState } from "react";
 import FoodList from "./components/FoodList.jsx";
-import mockItems from "./data/items.json";
+import { getFoodList } from "./api.js";
 
 function App() {
   const [order, setOrder] = useState("createdAt");
-  const [items, setItems] = useState(mockItems);
+  const [items, setItems] = useState([]);
 
   const sortedItems = items.sort((a, b) => b[order] - a[order]);
 
@@ -17,11 +17,17 @@ function App() {
     setItems(nextItems);
   };
 
+  const handleLoadClick = async () => {
+    const { foods } = await getFoodList();
+    setItems(foods);
+  };
+
   return (
     <div>
       <button onClick={handleNewestClick}>최신순</button>
       <button onClick={handleCalorieClick}>칼로리순</button>
       <FoodList items={sortedItems} onDelete={handleDelete} />
+      <button onClick={handleLoadClick}>음식 불러오기</button>
     </div>
   );
 }
